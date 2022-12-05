68ee654a41d1026d1aaad15cf73dd5af7bd50535
Denchik117 <deniusproficus@gmail.com>
2022-12-05 21:39:17 +0100

    666

--- scroll.js
+++
@@ -1,8 +0,0 @@
-document.backdrop.style.position = 'fixed';
-document.backdrop.style.top = `-${window.scrollY}px`;
-document.backdrop.style.position = '';
-document.backdrop.style.top = '';
-const top = document.body.style.top;
-document.backdrop.style.position = '';
-document.backdrop.style.top = '';
-window.scrollTo(0, parseInt(scrollY || '0') * -1);
